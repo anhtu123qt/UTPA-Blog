19ed31201653392e0a185dd13013b7a8da50e3d5
anhtu123qt <anhtu123qt@gmail.com>
2021-09-03 15:50:37 +0700

    UTPA blog

---
+++ resources/views/index.blade.php
@@ -0,0 +1,296 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet">
+    <title>UTPANews</title>
+    <style>
+        a {
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+{{--navbar--}}
+   <div class="container">
+       <nav class="navbar navbar-expand-lg">
+           <a href="#" class="navbar-brand">UTPANews</a>
+           <div class="collapse navbar-collapse">
+               <ul class="navbar-nav mx-3">
+                   <li class="nav-item">
+                       <a href="#" class="nav-link">Xã hội</a>
+                   </li>
+                   <li class="nav-item">
+                       <a href="#" class="nav-link">Thế giới</a>
+                   </li>
+                   <li class="nav-item">
+                       <a href="#" class="nav-link">Kinh doanh</a>
+                   </li>
+                   <li class="nav-item">
+                       <a href="#" class="nav-link">Công nghệ</a>
+                   </li>
+                   <li class="nav-item">
+                       <a href="#" class="nav-link">Sức khỏe</a>
+                   </li>
+                   <li class="nav-item">
+                       <a href="#" class="nav-link">Thể thao</a>
+                   </li>
+                   <li class="nav-item">
+                       <a href="#" class="nav-link">Giải trí</a>
+                   </li>
+                   <li class="nav-item">
+                       <a href="#" class="nav-link">Đời sống</a>
+                   </li>
+                   <li class="nav-item">
+                       <a href="#" class="nav-link">Xuất bản</a>
+                   </li>
+                   <li class="nav-item">
+                       <a href="#" class="nav-link">Sách</a>
+                   </li>
+                   <div class="vr"></div>
+               </ul>
+           </div>
+       </nav>
+       <hr>
+   </div>
+{{--endnavbar--}}
+   <section>
+       <div class="container">
+{{--           hoi tiet cac thu--}}
+       </div>
+   </section>
+   <section>
+       <div class="container">
+           <div class="row">
+               <div class="col-3">
+                   <div class="card border-0">
+                       <div class="row g-0">
+                           <div class="col-md-4">
+                               <img src="{{asset('images/avatar.png')}}" class="img-fluid rounded-start" alt="...">
+                           </div>
+                           <div class="col-md-8">
+                               <div class="card-body">
+                                   <a href=""><h6 class="card-title">VN 10 - 0 Ả Rập: Quá dễ cho tuyển VN</h6></a>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+                   <hr>
+                   <div class="card border-0">
+                       <div class="row g-0">
+                           <div class="col-md-4">
+                               <img src="{{asset('images/avatar.png')}}" class="img-fluid rounded-start" alt="...">
+                           </div>
+                           <div class="col-md-8">
+                               <div class="card-body">
+                                   <a href=""><h6 class="card-title">VN 10 - 0 Ả Rập: Quá dễ cho tuyển VN</h6></a>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+                   <hr>
+                   <div class="card border-0">
+                       <div class="row g-0">
+                           <div class="col-md-4">
+                               <img src="{{asset('images/avatar.png')}}" class="img-fluid rounded-start" alt="...">
+                           </div>
+                           <div class="col-md-8">
+                               <div class="card-body">
+                                   <a href=""><h6 class="card-title">VN 10 - 0 Ả Rập: Quá dễ cho tuyển VN</h6></a>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+                   <hr>
+                   <div class="card border-0">
+                       <div class="row g-0">
+                           <div class="col-md-4">
+                               <img src="{{asset('images/avatar.png')}}" class="img-fluid rounded-start" alt="...">
+                           </div>
+                           <div class="col-md-8">
+                               <div class="card-body">
+                                   <a href=""><h6 class="card-title">VN 10 - 0 Ả Rập: Quá dễ cho tuyển VN</h6></a>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+               <div class="col-6">
+                   <div class="card border-0">
+                       <img style="height:328px;" src="{{asset('images/avatar.png')}}" alt="">
+                       <div class="card-body">
+                           <a href=""><h2 class="card-title">Thứ trưởng Bộ Y tế: Sớm kêu gọi F0 khỏi bệnh tham gia chống dịch</h2></a>
+                           <p class="card-text">Thứ trưởng Bộ Y tế Nguyễn Trường Sơn cho biết việc này hoàn toàn dựa trên tinh thần tự nguyện và sẽ tạo điều kiện tốt nhất để người dân tham gia.</p>
+                       </div>
+                   </div>
+               </div>
+               <div class="col-3">
+                   <div class="card border-0">
+                       <img style="height:175px" src="{{asset('images/avatar.png')}}" alt="">
+                       <div class="card-body pt-2">
+                           <a href=""><h6 class="card-title">Thứ trưởng Bộ Y tế: Sớm kêu gọi F0 khỏi bệnh tham gia</h6></a>
+                       </div>
+                   </div>
+                   <div class="card border-0">
+                       <img style="height:175px" src="{{asset('images/avatar.png')}}" alt="">
+                       <div class="card-body pt-2">
+                           <a href=""><h6 class="card-title">Thứ trưởng Bộ Y tế: Sớm kêu gọi F0 khỏi bệnh tham gia</h6></a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </section>
+{{--multimedia--}}
+   <section class="bg-success text-light m-4">
+       <div class="container">
+           <nav class="navbar navbar-expand-lg">
+               <h4 class="navbar-brand"><a></a>MULTIMEDIA</h4>
+               <div class="collapse navbar-collapse">
+                   <ul class="navbar-nav mx-4 pt-2">
+                       <li class="nav-item">
+                           <h6 href="#" class="nav-link text-light">VIDEO</h6>
+                       </li>
+                       <li class="nav-item px-4">
+                           <h6 href="#" class="nav-link text-light">LONGFORM</h6>
+                       </li>
+                       <li class="nav-item">
+                           <h6 href="#" class="nav-link text-light">VOICES</h6>
+                       </li>
+                       <li class="nav-item px-4">
+                           <h6 href="#" class="nav-link text-light">LENS</h6>
+                       </li>
+                       <li class="nav-item">
+                           <h6 href="#" class="nav-link text-light">STORY</h6>
+                       </li>
+                       <li class="nav-item px-4">
+                           <h6 href="#" class="nav-link text-light">QUIZZ</h6>
+                       </li>
+                   </ul>
+               </div>
+           </nav>
+           <div class="row">
+               <div class="col-6">
+                   <div class="card border-0">
+                       <img style="height:328px;" src="{{asset('images/avatar.png')}}" alt="">
+                       <div class="card-body bg-success">
+                           <a href=""><h5 class="card-title text-light">Thứ trưởng Bộ Y tế: Sớm kêu gọi F0 khỏi bệnh tham gia chống dịch</h5></a>
+                           <p class="card-text">Thứ trưởng Bộ Y tế Nguyễn Trường Sơn cho biết việc này hoàn toàn dựa trên tinh thần tự nguyện và sẽ tạo điều kiện tốt nhất để người dân tham gia.</p>
+                       </div>
+                   </div>
+               </div>
+               <div class="col-6">
+                   <div class="row row-cols-md-2">
+                       <div class="col">
+                           <div class="card border-0">
+                               <img style="height:170px;width:261px;" src="{{asset('images/avatar.png')}}" class="card-img-top" alt="">
+                               <div class="card-body bg-success">
+                                   <h6 class="card-title">Tiêm vaccine Covid-19 có ảnh hưởng tới kế hoạch mang thai?</h6>
+                               </div>
+                           </div>
+                       </div>
+                       <div class="col">
+                           <div class="card border-0">
+                               <img style="height:170px;width:261px;" src="{{asset('images/avatar.png')}}" class="card-img-top" alt="">
+                               <div class="card-body bg-success">
+                                   <h6 class="card-title">Tiêm vaccine Covid-19 có ảnh hưởng tới kế hoạch mang thai?</h6>
+                               </div>
+                           </div>
+                       </div>
+                       <div class="col">
+                           <div class="card border-0">
+                               <img style="height:170px;width:261px;" src="{{asset('images/avatar.png')}}" class="card-img-top" alt="">
+                               <div class="card-body bg-success">
+                                   <h6 class="card-title">Tiêm vaccine Covid-19 có ảnh hưởng tới kế hoạch mang thai?</h6>
+                               </div>
+                           </div>
+                       </div>
+                       <div class="col">
+                           <div class="card border-0">
+                               <img style="height:170px;width:261px;" src="{{asset('images/avatar.png')}}" class="card-img-top" alt="">
+                               <div class="card-body bg-success">
+                                   <h6 class="card-title">Tiêm vaccine Covid-19 có ảnh hưởng tới kế hoạch mang thai?</h6>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </section>
+{{--endmulti--}}
+{{---------------------}}
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-8">
+                <div class="card mb-3 border-0">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="{{asset('images/avatar.png')}}" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Hàn Quốc 0-0 Iraq: Chủ nhà chơi bế tắc</h5>
+                                <p class="card-text"><small class="text-muted">2 GIỜ TRƯỚC</small></p>
+                                <p class="card-text"><small class="text-muted">Son Heung-min và đồng đội chưa có nhiều khoảng trống để phối hợp trong vùng cấm của tuyển Iraq sau 20 phút đầu.</small></p>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="{{asset('images/avatar.png')}}" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body border-0">
+                                <h5 class="card-title">Hàn Quốc 0-0 Iraq: Chủ nhà chơi bế tắc</h5>
+                                <p class="card-text"><small class="text-muted">2 GIỜ TRƯỚC</small></p>
+                                <p class="card-text"><small class="text-muted">Son Heung-min và đồng đội chưa có nhiều khoảng trống để phối hợp trong vùng cấm của tuyển Iraq sau 20 phút đầu.</small></p>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="{{asset('images/avatar.png')}}" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body border-0">
+                                <h5 class="card-title">Hàn Quốc 0-0 Iraq: Chủ nhà chơi bế tắc</h5>
+                                <p class="card-text"><small class="text-muted">2 GIỜ TRƯỚC</small></p>
+                                <p class="card-text"><small class="text-muted">Son Heung-min và đồng đội chưa có nhiều khoảng trống để phối hợp trong vùng cấm của tuyển Iraq sau 20 phút đầu.</small></p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-4">
+                <div style="background-color:#f7f7f7" class="hstack gap-3">
+{{--                    <div class="bg-light float-start ">First item</div>--}}
+{{--                    <div class="vr"></div>--}}
+{{--                    <div class="bg-light float-end ">Third item</div>--}}
+                    <div class="float-start">
+                        <small>CHỨNG KHOÁN</small>
+                    </div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <div class="vr"></div>
+                    <div class="float-end">
+                        <small>TỈ GIÁ USD</small>
+                    </div>
+                </div>
+                <div class="p-5">
+                    <img src="{{asset('images/avatar.png')}}" class="img-fluid rounded-start" alt="...">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+</body>
+</html>
